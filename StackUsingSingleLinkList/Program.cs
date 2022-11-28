b4d1cd853ec513755c5edea2d46248df13512800
LISA <ASUS@LAPTOP-67G15PD7>
2022-11-28 16:24:50 +0700

    add public node

--- StackUsingSingleLinkList/Program.cs
+++ StackUsingSingleLinkList/Program.cs
@@ -9,6 +9,12 @@ namespace StackUsingSingleLinkList
     class node
     {
         public int info;
+        public node next;
+        public node(int i, node n)
+        {
+            info = i;
+            next = n;
+        }
     }
     internal class Program
     {
